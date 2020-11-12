843b08ba3d78a8e1a9d4d58a2972274372001033
Wim Wauters <wauterw@cisco.com>
2020-11-12 14:46:52 +0100

    Initial commit

--- ESXi_ARM_VMTools/install_openvm_tools.sh
+++ ESXi_ARM_VMTools/install_openvm_tools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt update -y && apt upgrades
+apt update -y && apt upgrade -y
 apt install -y git automake make gobjc++ libtool pkg-config libmspack-dev libglib2.0-dev libpam0g-dev libssl-dev libxml2-dev libxmlsec1-dev libx11-dev libxext-dev libxinerama-dev libxi-dev libxrender-dev libxrandr-dev libxtst-dev libgdk-pixbuf2.0-dev libgtk-3-dev libgtkmm-3.0-dev
 git clone https://github.com/vmware/open-vm-tools.git
 cd open-vm-tools/open-vm-tools/
